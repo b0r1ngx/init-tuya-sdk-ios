f3b2a5f57003556e162aecd5d888d3bb509d2571
Ivanov Kirill A <ghztrikz1337@gmail.com>
2024-03-05 21:09:04 +0300

    Initial Commit

---
+++ TuyaHuya/TuyaHuyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuyaHuyaApp.swift
+//  TuyaHuya
+//
+//  Created by Кирилл Иванов on 05.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct TuyaHuyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
